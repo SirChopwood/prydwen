ddc244a3c1f58572e5f17757eea0c5ab356f6d65
Ramiris <sirchopwood@gmail.com>
2024-06-06 22:52:54 +0100

    Moved Images into images folder

--- src/views/home.vue
+++ src/views/home.vue
@@ -9,7 +9,7 @@
   </div>
   <div class="mt-8 text-2xl text-center text-secondary">My latest work:</div>
   <div class="w-full mt-2 grid grid-cols-1 px-8 justify-items-center">
-    <ProjectThumbnail title="Adventurer's Guild Manager" subtitle="Master's Final Project, Gameplay Programming & User Interfaces" image="/agm/LogoPart1.png" link="/projects/agm"/>
+    <ProjectThumbnail title="Adventurer's Guild Manager" subtitle="Master's Final Project, Gameplay Programming & User Interfaces" image="/images/agm/LogoPart1.png" link="/projects/agm"/>
   </div>
   <div class="mt-8 text-2xl text-center text-secondary">My other projects:</div>
   <div class="w-full mt-2 grid grid-cols-4 gap-8 px-4 justify-items-center">
