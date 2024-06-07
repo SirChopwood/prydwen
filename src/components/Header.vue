72187db240817ac489ef550c743162e6006d562c
Ramiris <sirchopwood@gmail.com>
2024-06-07 08:45:00 +0100

    Add "prydwen/" prefix to paths

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="fixed w-full h-14 top-0 bg-neutral-800 text-primary border-b-4 border-secondary text-xl drop-shadow-lg">
     <div class="absolute top-0 flex items-center justify-center w-full h-full gap-8">
-      <Headerbutton text="Portfolio" link="/" />
-      <Headerbutton text="About Me" link="/about" />
+      <Headerbutton text="Portfolio" link="/prydwen/" />
+      <Headerbutton text="About Me" link="/prydwen/about" />
     </div>
     <div class="absolute top-0 flex items-center h-full w-fit">
       <a href="/"><div class="pl-4 text-2xl font-bold">Louis Mayes</div></a>
