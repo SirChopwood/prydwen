49b372d0c597b9486c0cfc88911aac6e8492df18
Ramiris <sirchopwood@gmail.com>
2024-07-29 16:43:57 +0100

    Title Bar Redo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="fixed w-full h-14 top-0 bg-neutral-800 text-primary border-b-4 border-secondary text-xl drop-shadow-lg">
-    <div class="absolute top-0 flex items-center justify-center w-full h-full gap-8">
+  <div class="fixed w-full h-fit py-1 md:py-2 top-0 bg-neutral-800 text-primary border-b-4 border-secondary text-xl drop-shadow-lg">
+    <div class="inline-flex items-center h-full w-fit md:ml-4 md:float-start">
+      <a href="/#/"><div class="text-2xl font-bold">Louis Mayes</div></a>
+      <div class="pl-4 italic text-neutral-400">Technical Artist</div>
+    </div>
+    <div class="inline-flex items-center md:mr-4 md:float-end w-fit h-full gap-8">
       <Headerbutton text="Portfolio" link="/#/" />
       <Headerbutton text="About Me" link="/#/about" />
     </div>
-    <div class="absolute top-0 flex items-center h-full w-fit">
-      <a href="/#/"><div class="pl-4 text-2xl font-bold">Louis Mayes</div></a>
-      <div class="pl-4 italic text-neutral-400">Technical Artist</div>
-    </div>
+  </div>
+  <div class="fixed w-full h-32 top-0 flex justify-center pointer-events-none">
+    <img src="/images/logo.png" class="hidden md:block h-24 w-auto md:ml-4">
   </div>
 </template>
 
