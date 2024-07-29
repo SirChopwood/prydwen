d9cdb2d2c1758cc9fb48b2a7f67a944efb9ff4d4
Ramiris <sirchopwood@gmail.com>
2024-07-29 20:01:16 +0100

    Halftone Dots Oo.

--- src/components/ProjectThumbnail.vue
+++ src/components/ProjectThumbnail.vue
@@ -3,9 +3,9 @@ defineProps(['title', 'subtitle', 'image', 'link'])
 </script>
 
 <template>
-  <a :href="link" class="group hover:text-white rounded-lg border-2 border-neutral-600 hover:border-secondary w-full max-w-[500px] h-52 text-pretty relative transition-all duration-400 ease-in-out">
+  <a :href="link" class="group hover:text-white rounded-lg border-2 border-neutral-600 hover:border-secondary w-full max-w-[500px] h-52 text-pretty relative transition-all duration-400 ease-in-out bg-black">
     <div class="bg-border rounded-lg bg-center bg-cover group-hover:blur-sm w-full h-full object-center z-10" :style="'background-image: url('+image+')'"></div>
-    <div class="absolute p-4 top-0 w-full h-fit md:h-full opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity bg-black rounded-lg bg-opacity-50">
+    <div class="absolute p-4 top-0 w-full h-fit md:h-full opacity-100 md:opacity-0 group-hover:opacity-100 transition-opacity bg-black rounded-lg bg-opacity-80">
       <div class="text-start text-2xl font-bold text-primary">{{title}}</div>
       <div class="pt-2 text-xl italic text-neutral-200 hidden md:block">{{subtitle}}</div>
     </div>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,16 @@
 <template>
-  <div id="background" class="fixed w-full h-full top-0 left-0"></div>
+  <div id="background" class="fixed w-full h-full top-0 left-0 bg-secondary bg-blend-screen">
+    <div class="w-full h-full halftone"></div>
+  </div>
   <div class="flex my-40 md:my-80 w-full h-1/6 justify-center">
-    <div class="relative text-left text-7xl top-10 left-2 md:left-6 lg:left-16">
-      <div class="absolute text-left text-3xl -top-10 -left-2 md:-left-12 lg:-left-32">
+    <div class="relative text-left text-7xl top-10 left-2 md:left-6 lg:left-16 p-4 bg-black bg-opacity-80 rounded-lg">
+      <div class="absolute text-left text-3xl -top-10 -left-2 md:-left-12 lg:-left-32 p-4 bg-black bg-opacity-80 rounded-lg">
         Hello there...
       </div>
       I'm <span class="text-primary">Louis</span>
     </div>
   </div>
-  <div class="w-full mt-8 text-3xl text-secondary text-left border-b-2 border-secondary font-black">My Portfolio</div>
+  <div class="relative w-full mt-8 text-3xl text-primary text-left border-b-2 border-primary font-black bg-black bg-opacity-80 rounded-t-lg">My Portfolio</div>
   <div class="w-full mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4 justify-items-center">
     <ProjectThumbnail title="Adventurer's Guild Manager" subtitle="Master's Final Project, Gameplay Programming & User Interfaces" image="/images/agm/LogoPart1.png" link="/#/projects/agm">
       <div class="absolute bottom-0 w-full px-2 py-0.5 text-xl font-black bg-cyan-800 border-2 border-cyan-400 rounded-b-md text-cyan-400">NEW</div>
@@ -32,3 +34,12 @@ export default {
   components: {ProjectThumbnail}
 }
 </script>
+
+<style>
+  .halftone {
+    position: absolute;
+    background: linear-gradient(to bottom, #777777, #ffffff), radial-gradient( 20px at 50% 50%, #fff, #000 ) top left / 30px 30px, #c940af;
+    background-blend-mode: multiply;
+    filter: brightness(1.3) blur(0px) contrast(30) saturate(0.7);
+  }
+</style>
