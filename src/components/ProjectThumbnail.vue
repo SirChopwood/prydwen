06e2339832ccd8feef9b2f741a5685fc0abd9071
Ramiris <sirchopwood@gmail.com>
2024-07-29 15:32:23 +0100

    Make Thumbnails screen size reactive

--- src/components/ProjectThumbnail.vue
+++ src/components/ProjectThumbnail.vue
@@ -3,11 +3,11 @@ defineProps(['title', 'subtitle', 'image', 'link'])
 </script>
 
 <template>
-  <a :href="link" class="group hover:text-white rounded-lg border-2 border-neutral-600 hover:border-secondary w-[20vw] h-[20vh] text-pretty relative transition-all duration-400 ease-in-out">
-    <div class="bg-border bg-center bg-cover group-hover:blur-sm w-full h-full object-center z-10" :style="'background-image: url('+image+')'"></div>
-    <div class="absolute p-4 top-0 w-full h-full opacity-0 group-hover:opacity-100 transition-opacity bg-black rounded-lg bg-opacity-50">
+  <a :href="link" class="group hover:text-white rounded-lg border-2 border-neutral-600 hover:border-secondary w-full max-w-[500px] h-52 text-pretty relative transition-all duration-400 ease-in-out">
+    <div class="bg-border rounded-lg bg-center bg-cover group-hover:blur-sm w-full h-full object-center z-10" :style="'background-image: url('+image+')'"></div>
+    <div class="absolute p-4 top-0 w-full h-fit lg:h-full opacity-100 lg:opacity-0 group-hover:opacity-100 transition-opacity bg-black rounded-lg bg-opacity-50">
       <div class="text-start text-2xl font-bold text-primary">{{title}}</div>
-      <div class="pt-2 text-xl italic text-neutral-200">{{subtitle}}</div>
+      <div class="pt-2 text-xl italic text-neutral-200 hidden lg:block">{{subtitle}}</div>
     </div>
     <slot />
   </a>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="background" class="fixed w-full h-full top-0 left-0"></div>
   <div class="flex my-80 w-full h-1/6 justify-center">
-    <div class="relative text-left text-7xl top-10 left-16">
-      <div class="absolute text-left text-3xl -top-10 -left-32">
+    <div class="relative text-left text-7xl top-10 left-2 md:left-6 lg:left-16">
+      <div class="absolute text-left text-3xl -top-10 -left-2 md:-left-12 lg:-left-32">
         Hello there...
       </div>
       I'm <span class="text-primary">Louis</span>
@@ -13,7 +13,7 @@
     <ProjectThumbnail title="Adventurer's Guild Manager" subtitle="Master's Final Project, Gameplay Programming & User Interfaces" image="/images/agm/LogoPart1.png" link="/#/projects/agm"/>
   </div>
   <div class="mt-8 text-2xl text-center text-secondary">My other projects:</div>
-  <div class="w-full mt-2 grid grid-cols-4 gap-8 px-4 justify-items-center">
+  <div class="w-full mt-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4 justify-items-center">
     <ProjectThumbnail title="Munitions Officer Simulator" subtitle="Game Feel and Polish" image="/images/mos/SplashScreen.png" link="/#/projects/mos"/>
     <ProjectThumbnail title="Versus Tower Defence" subtitle="Online Multiplayer Prototype" link="/#/projects/vstd">
       <div class="absolute bottom-0 w-full px-2 py-1 bg-amber-800 border-2 border-amber-400 rounded-b-md text-amber-400 opacity-50 group-hover:opacity-100">Work-In-Progress</div>
