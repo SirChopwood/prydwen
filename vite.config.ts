fcc088893e20f6e7c14dcd584f1d597ed5c3d6a4
Ramiris <sirchopwood@gmail.com>
2024-11-15 23:52:36 +0000

    Move 404 to public because fuck Vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  appType: "mpa",
+  appType: "spa",
   plugins: [
     vue(),
     vueJsx(),
