31456d4e903d9f0046187c434878acabbae0af7e
Ramiris <sirchopwood@gmail.com>
2024-11-15 23:01:11 +0000

    Fixed Github Pages SPA Routing?

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {header: true}
     },
     {
       path: '/about',
@@ -15,44 +16,72 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/about.vue')
+      component: () => import('../views/about.vue'),
+      meta: {header: true}
     },
     {
-      path: '/projects/agm',
-      name: 'agm',
-      component: () => import('../views/projects/agm.vue')
-    },
-    {
-      path: '/projects/mos',
-      name: 'mos',
-      component: () => import('../views/projects/mos.vue')
-    },
-    {
-      path: '/projects/sas',
-      name: 'sas',
-      component: () => import('../views/projects/sas.vue')
-    },
-    {
-      path: '/projects/dvz',
-      name: 'dvz',
-      component: () => import('../views/projects/dvz.vue')
-    },
-    {
-      path: '/rrm',
-      name: 'rrm_auth',
-      component: () => import('../views/rrm/auth.vue')
-    },
-    {
-      path: '/rrm/panel',
-      name: 'rrm_panel',
-      component: () => import('../views/rrm/panel.vue')
+      path: '/projects',
+      name: 'projects',
+      meta: {header: true},
+      children: [
+        {
+          path: 'agm',
+          name: 'agm',
+          component: () => import('../views/projects/agm.vue')
+        },
+        {
+          path: 'mos',
+          name: 'mos',
+          component: () => import('../views/projects/mos.vue')
+        },
+        {
+          path: 'sas',
+          name: 'sas',
+          component: () => import('../views/projects/sas.vue')
+        },
+        {
+          path: 'dvz',
+          name: 'dvz',
+          component: () => import('../views/projects/dvz.vue')
+        },
+        {
+          path: '/rrm',
+          name: 'rrm',
+          meta: {header: false},
+          children: [
+            {
+              path: '',
+              name: 'rrm_auth',
+              component: () => import('../views/rrm/auth.vue')
+            },
+            {
+              path: 'panel',
+              name: 'rrm_panel',
+              component: () => import('../views/rrm/panel.vue')
+            },
+            {
+              path: 'overlay',
+              name: 'rrm_overlay',
+              component: () => import('../views/rrm/overlay.vue')
+            }
+          ]
+        },
+      ]
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'notfound',
-      component: HomeView
+      component: HomeView,
+      meta: {header: true}
     }
   ]
 })
-
+router.beforeEach((to, from) => {
+  let path = localStorage.getItem('path');
+  console.log(path)
+  if(path) {
+    localStorage.removeItem('path');
+    return { path: path, replace: true}
+  }
+})
 export default router
