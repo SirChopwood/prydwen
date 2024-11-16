f80164e85f3d5b1ae63953588b069d4a0661ce04
Ramiris <sirchopwood@gmail.com>
2024-11-16 01:12:33 +0000

    Fixed some merge fucky wuckys

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import './assets/style.css'
 import { createApp } from 'vue'
+import { createHead } from '@unhead/vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+const head = createHead()
+app.use(head)
 app.use(router)
 
 app.mount('#app')
